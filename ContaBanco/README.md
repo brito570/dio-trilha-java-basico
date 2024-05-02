635002b6c3552238d87f355dd26f237df46b0d52
brito570 <lucas.alves.lima.brito@gmail.com>
2024-05-02 14:34:02 -0300

    add: Projeto completo

---
+++ ContaBanco/README.md
@@ -0,0 +1,3 @@
+## Desafio conta terminal ☕
+
+Desafio proposto pela [DIO](https://www.dio.me/) para conclusão do módulo de sintaxe JAVA para o Bootcamp Santander.
---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        /** TODO: Conhecer e importar a classe Scanner
+        // Exibir as mensagens para o nosso usuário
+        // Obter pela Scanner os valores digitados no terminal
+         Exibir a mensagem conta criada*/
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem-vindo(a) à página de cadastro do Luk's Bank's");
+        System.out.println("Para inciarmos, por favor, digite o seu nome completo: ");
+        String nomeCliente = sc.nextLine();
+
+        System.out.printf("Obrigado pela informação, %s. ", nomeCliente);
+
+        System.out.println("Agora, digite o número da conta: ");
+        int numeroDaConta = sc.nextInt();
+
+        System.out.println("Digite o número da Agência com o hífen para separar o dígito: ");
+        String agencia = sc.next();
+
+        System.out.println("Para válidar o cadastro, digite o valor a ser inserido no seu saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.print("Olá, "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroDaConta + " e seu saldo R$ " + saldo + " já está disponível para saque");
+
+        sc.close();
+
+    }
+}
